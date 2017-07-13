59b21cc03da758a0eac1271e84baaed6c1135466
Renato D. Prado <renatoprado21@gmail.com>
2017-07-13 10:21:26 -0300

    Remover console log

--- injector.js
+++ injector.js
@@ -4,7 +4,6 @@ var WorkoutUseCaseInteractor = require('./src/scenes/workoutUseCaseInteractor')
 var WorkoutController = require('./src/scenes/workoutController')
 
 exports.inject = function(router) {
-    console.log('Injecting')
     var workoutRepository = new WorkoutRepository(null)
     var workoutUseCaseInteractor = new WorkoutUseCaseInteractor(workoutRepository);
     var workoutController = new WorkoutController(router, workoutUseCaseInteractor);
