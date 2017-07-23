0d9efdf169e46611a5586edd38dc410719e646ec
Renato D. Prado <renatoprado21@gmail.com>
2017-07-23 13:33:18 -0300

    Add, delete, list with VUE

--- src/scenes/workoutsController.js
+++ src/scenes/workoutsController.js
@@ -11,6 +11,12 @@ var WorkoutsController = function WorkoutsController(){
 
     function makeRouts(router, workoutsUseCaseInteractor)
     {
+        function sendWorkouts(res) {
+            var done = function(viewModel){  
+                res.json(viewModel.workouts);
+            }
+            workoutsUseCaseInteractor.getWorkouts(done);
+        }
 
         router.get('/vuetest', function(req, res, next) {
             res.json({car: "HAHAHAHA", name: "this is Working, just need to create an http server to run vue htmls and install Allow-Control-Allow-Origin extension on chrome to allow cross origin requests"});
@@ -18,11 +24,7 @@ var WorkoutsController = function WorkoutsController(){
         });
 
         router.get('/workouts', function(req, res, next) {
-            var done = function(viewModel){  
-                res.json(viewModel.workouts);
-                // res.render('workouts', {'workouts': viewModel.workouts})
-            }
-            workoutsUseCaseInteractor.getWorkouts(done);
+            sendWorkouts(res);
         });
 
         router.post('/workouts/add', function(req, res, next) {
@@ -36,15 +38,18 @@ var WorkoutsController = function WorkoutsController(){
             }
 
             workoutsUseCaseInteractor.addWorkout(requestModel);
-            //res.send('Post page');
+
+            sendWorkouts(res);
         });
 
-        router.post('/workouts/delete', function(req, res, next){
+        router.post('/workouts/remove', function(req, res, next){
             var requestModel = {
                 ID: req.body.ID,
             }
             workoutsUseCaseInteractor.removeWorkout(requestModel);
-            //res.send('Delete page');
+
+            sendWorkouts(res);
+
         });
     }
 
